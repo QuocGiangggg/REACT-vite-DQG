8a354a81bb2abb94bfdbb0ccbb7c99eb182f78da
QuocGiangggg <dangquocgiang2002@gmail.com>
2025-05-13 16:16:46 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,15 @@
 // () => { }
 //component = html + css +js
+import './style.css';
 const MyComponent = () => {
   return (
-    <div>Giang va Hanh la hai dua ban than </div> // tag
+    <>
+        <div>Giang va Hanh la hai dua ban than </div>
+        <div className ="Child"
+         style = {
+            {borderRadius: "10px"}
+         }>Cap doi Huy Diet</div>
+    </>
   );
   }
 
