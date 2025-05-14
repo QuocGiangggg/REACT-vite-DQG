d1964153d6b73d7dc76e68a708a45c801b0df42a
QuocGiangggg <dangquocgiang2002@gmail.com>
2025-05-14 11:50:44 +0700

    video 36

--- src/components/todo/todonew.jsx
+++ src/components/todo/todonew.jsx
@@ -2,11 +2,23 @@
 const Todonew =(props) =>{
     console.log('check: ',props)
     const {addNewTodo} = props;
-    addNewTodo("Dang Quoc Giang")
+    //addNewTodo("Dang Quoc Giang")
+    const handleClick =() => {
+        alert("Click me")
+    }
+    const handleonChange = (name) => {
+        console.log("check: ",name)
+    }
     return (
         <div className='todo-new'>
-        <input type ="text"/>
-        <button>Add</button>
+        <input 
+            type ="text"
+            onChange ={(evnet) => handleonChange(evnet.target.value)}
+        />
+        <button 
+            style ={{cursor: "pointer"}}
+            onClick={handleClick}
+        >Add</button>
       </div>
     )
 }
